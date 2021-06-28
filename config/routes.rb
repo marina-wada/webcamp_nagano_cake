d122fd86c8f36bedb71dee5d046663918ca38ee3
EC2 Default User <ec2-user@ip-172-31-0-175.ec2.internal>
2021-06-28 23:57:51 +0000

    [Update]ログイン画面ヘッダー追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     root 'homes#top'
-    resources :admin
   end
 end
