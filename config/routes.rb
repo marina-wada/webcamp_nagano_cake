0c33cd7348b0738408cbea5d415be908a65e8143
EC2 Default User <ec2-user@ip-172-31-0-175.ec2.internal>
2021-07-07 14:08:47 +0000

    [Update]items

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
   namespace :admin do
-    # get 'items/index'
-    # get 'items/new'
-    # get 'items/create'
-    # get 'items/show'
-    # get 'items/edit'
-    # get 'items/update'
-    resource :items, only: [:index, :new, :create, :show, :edit, :update]
+    root 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
   devise_for :customers
   devise_for :admin, controllers: {
         sessions: 'admins/sessions'
       }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :admin do
-    root 'homes#top'
-  end
 end
