030c75e2b1ba52d532b9a07b6be350e94cee46f2
EC2 Default User <ec2-user@ip-172-31-0-175.ec2.internal>
2021-07-25 15:21:03 +0000

    [Update]public_item_index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all.page(params[:page]).per(8)
+    @items = Item.all.where(is_active: true).page(params[:page]).per(8)
     @item = Item.where(is_active: true)
     @genre = Genre.all
   end
