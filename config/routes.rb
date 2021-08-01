4469aeeecc17bc11d6e6dbd160d5af778e18f2a5
EC2 Default User <ec2-user@ip-172-31-0-175.ec2.internal>
2021-08-01 14:12:50 +0000

    [Update]public_order_index

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
     root 'homes#top'
     get 'about' => 'homes#about'
     resources :items, only: [:index, :show]
-    resources :orders, only: [:new, :confirm, :complete, :create, :index, :show]
+    resources :orders, only: [:new, :create, :index, :show]
     get 'orders/complete' => 'orders#complete'
     post 'orders/confirm' => 'orders#confirm'
-
+    resources:addresses, only: [:index, :edit, :create, :update, :destroy]
     resources :cart_items, only: [:index, :update, :destroy, :create] do
       collection do
         delete 'destroy_all'
