d9d640f100402cb55daeb9b790f419c0d2c25d3d
EC2 Default User <ec2-user@ip-172-31-0-175.ec2.internal>
2021-08-07 13:39:52 +0000

    [Update]public_customer_edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,6 +4,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
@@ -13,5 +14,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
