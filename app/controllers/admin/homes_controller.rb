d90934f3b246d191b8568624f32764f059d6afd0
EC2 Default User <ec2-user@ip-172-31-0-175.ec2.internal>
2021-08-15 13:46:47 +0000

    [Update]admin_customer_edit

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,8 +1,14 @@
 class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
-    @orders = Order.all.page(params[:page]).per(10)
-
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "admin/sessions" && path[:action] == "new"
+      @orders = Order.all.page(params[:page]).per(10)
+    elsif path[:controller] == "application" && path[:action] == "application"
+      @orders = Order.all.page(params[:page]).per(10)
+    else path[:controller] == "admin/customers" && path[:action] == "show"
+      @orders = Order.find(customer)
+    end
   end
 
 end
